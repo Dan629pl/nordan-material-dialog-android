91516c0b4cdbc2768caae64c2fb83a0ea461252a
nordan <daniel.owczarczyk.pl@gmail.com>
2020-07-19 21:01:30 +0200

    init sample App

---
+++ sampleapp/src/main/java/com/nordan/sampleapp/DefaultDialog.java
@@ -0,0 +1,6 @@
+package com.nordan.sampleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DefaultDialog extends AppCompatActivity {
+}
---
+++ sampleapp/src/main/java/com/nordan/sampleapp/DialogCreator.java
@@ -0,0 +1,15 @@
+package com.nordan.sampleapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DialogCreator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_creator_activity);
+    }
+}
